5d4f1ee2f1111cdc7326f914484ed25ca0dc7b50
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-07 23:06:59 -0500

    Uh oh first try alert?

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - A swap function for bubble sort
+ * @array: the array elements
+ * @i: the element
+ * @j: the element to swapped with
+ * Return: RELAX
+ */
+void swap(int* array, int i, int j)
+{
+int temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+}
+/**
+ * bubble_sort - A bubble sort implementation
+ * @array: array of integars
+ * @size: size of list
+ * Return: Ordered list
+*/
+void bubble_sort(int *array, size_t size)
+{
+  size_t i, j;
+
+  for (i = 0; i < size - 1; i++)
+	  for (j = 0; j < size - i - 1; j++)
+		  if (array[j] > array[j + 1])
+      {
+				  swap(array, j, j + 1);
+          print_array(array, size);
+      }
+}

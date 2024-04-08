0bf349eca732b72399dbe3c9a6635e64b0884bfe
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-08 18:53:02 -0500

    Does this work betty? HUH

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,35 @@
 #include "sort.h"
 
-void sort_swap(listint_t **list, listint_t *new_node)
-{
-  listint_t* current;
-
-  if (*list == NULL || (*list)->n >= new_node->n)
-  {
-    new_node->next = *list;
-    *list = new_node;
-  }
-  else
-  {
-    current = *list;
-    while (current->next != NULL && current->next->n < new_node->n)
-    {
-      current = current->next;
-    }
-    new_node->next = current->next;
-    if (current->next != NULL)
-    current->next->prev = new_node;
-    current->next = new_node;
-    new_node->prev = current;
-  }
-}
 void insertion_sort_list(listint_t **list)
 {
-  listint_t* sorted = NULL;
-  listint_t* current = *list;
-  listint_t* next;
+	listint_t *current, *temp;
 
-  while (current != NULL)
-  {
-    next = current->next;
-    sort_swap(&sorted, current);
-    current = next;
-  }
-  *list = sorted;
-  print_list(*list);
-  }
+	if (!list)
+		return;
+
+	for (current = *list; current; current = current->next)
+	{
+		while (current->next && (current->next->n < current->n))
+		{
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+
+			if (current->prev)
+				current->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = current;
+
+			current->prev = temp;
+			temp->next = current;
+
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+	}
+}

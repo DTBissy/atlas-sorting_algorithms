ca6641062637ac8290df2020375f9b872792b563
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-19 18:05:23 -0500

    LEts see

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,15 +13,17 @@ for (i = 0; i < size - 1; i++)
 {
 	min = i;
 	for (j = i + 1; j < size; j++)
+  {
 	if (array[j] < array[min])
 	min = j;
+  }
 	if (min != i)
   {
 	temp = array[min];
 
 	array[min] = array[i];
 	array[i] = temp;
+  print_array(array, size);
 }
-	print_array(array, size);
 }
 }

f236bb3b5b31282755b828e4496e819fca7dc4a9
{DTBissy} <{isaactheedwards@gmail.com}>
2024-04-19 17:50:26 -0500

    Wtf

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,24 +2,25 @@
 
 void sel_swap(int *array, int min, int i)
 {
-    int temp = array[min];
-    array[min] =array[i];
-    array[i] = temp;
+	int temp = array[min];
+
+	array[min] = array[i];
+	array[i] = temp;
 }
 
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j;
-  size_t min;
+size_t i, j;
+size_t min;
 
-  for (i = 0; i < size - 1; i++)
-  {
-    min = i;
-    for (j = i + 1; j < size; j++)
-      if (array[j] < array[min])
-      min = j;
-    if (min != i)
-    sel_swap(array, min, i);
-    print_array(array, size);
-  }
- }
+for (i = 0; i < size - 1; i++)
+{
+	min = i;
+	for (j = i + 1; j < size; j++)
+	if (array[j] < array[min])
+	min = j;
+	if (min != i)
+	sel_swap(array, min, i);
+	print_array(array, size);
+}
+}
